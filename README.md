815e41568d4445e9e62f4ec48d020da887d1d8b6
Boglarka Weltz <weltz.boglarka@gmail.com>
2024-11-30 16:39:05 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+:heart: :star: :ribbon: :santa: :christmas_tree: Advent of Code 2024 :christmas_tree: :santa: :ribbon: :star: :heart:
---
+++ day.php
@@ -0,0 +1,36 @@
+<?php
+
+$input = "";
+
+$input = "";
+
+function day1($input) {
+  $lines = explode("\n", $input);
+  $res = 0;
+  for ($i = 0; $i < sizeof($lines); $i++) {
+    if () {
+      $res += ;
+    } else {
+      $res += ;
+    } 
+  }
+
+  return $res;
+}
+
+function day2($input) {
+  $lines = explode("\n", $input);
+  $res = 0;
+  for ($i = 0; $i < sizeof($lines); $i++) {
+    if () {
+      $res += ;
+    } else {
+      $res += ;
+    } 
+  }
+
+  return $res;
+}
+
+echo day1($input);
+//echo day2($input);
